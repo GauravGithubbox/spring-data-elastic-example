bd034a3eb520e66b887ef14ca48a686337bf444e
GauravGithubbox <Gaurav.srivastav@knoldus.com>
2022-07-18 18:38:48 +0530

    Initial push

---
+++ src/main/java/com/knoldus/repository/UsersRepository.java
@@ -0,0 +1,12 @@
+package com.knoldus.repository;
+
+import com.knoldus.model.Users;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface UsersRepository extends ElasticsearchRepository<Users, Long> {
+    List<Users> findByName(String text);
+
+    List<Users> findBySalary(Long salary);
+}
